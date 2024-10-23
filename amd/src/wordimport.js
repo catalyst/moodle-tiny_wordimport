4771bbff71549e1e8b76f7e8084c2e2497ca7745
Andre Menrath <andre.menrath@uni-graz.at>
2024-10-23 13:26:32 +0200

    add debug alert

--- amd/src/wordimport.js
+++ amd/src/wordimport.js
@@ -64,7 +64,7 @@ export const importWordFileHandler = async(editor) => {
         insertRawHtml(editor, content);
     } catch (error) {
         // TODO: display errors properly
-        window.console.log(error);
+        alert(error);
     }
 };
 
